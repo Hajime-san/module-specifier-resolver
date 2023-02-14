536055d02019fd2134eb8dba4d93d08b8a0e3c0b
Hajime-san <utd.c.r.d.s.a@gmail.com>
2023-02-14 10:13:42 +0900

    feat(deps): update std

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export * as path from 'https://deno.land/std@0.176.0/path/mod.ts';
-export { walk } from 'https://deno.land/std@0.176.0/fs/walk.ts';
+export * as path from 'https://deno.land/std@0.177.0/path/mod.ts';
+export { walk } from 'https://deno.land/std@0.177.0/fs/walk.ts';
 export { default as ts } from 'npm:typescript@beta';
-export * as cli from 'https://deno.land/std@0.176.0/flags/mod.ts';
-export * as io from 'https://deno.land/std@0.176.0/io/mod.ts';
+export * as cli from 'https://deno.land/std@0.177.0/flags/mod.ts';
+export * as io from 'https://deno.land/std@0.177.0/io/mod.ts';
 export * as fs from 'https://deno.land/std@0.177.0/fs/mod.ts';
